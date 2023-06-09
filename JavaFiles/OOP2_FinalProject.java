b452f4154a6920f027167dc529338c39c7e007c1
NateElison18 <122648132+NateElison18@users.noreply.github.com>
2023-06-08 19:53:13 -0600

    Update GUI OOP2_FinalProject.java

--- JavaFiles/OOP2_FinalProject.java
+++ JavaFiles/OOP2_FinalProject.java
@@ -2,63 +2,163 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
-
+import javafx.scene.layout.BorderPane;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class OOP2_FinalProject extends Application {
+    public OOP2_FinalProject() throws FileNotFoundException {
+    }
+
     public static void main(String [] args) {
         launch(args);
     }
+    // Import files and generate arraylists
+    File pitchersFile = new File("22PitcherStats.csv");
+    ArrayList<Pitcher> pitchers = generatePitcherArray(pitchersFile);
+    File hittersFile = new File("22HitterStats.csv");
+    ArrayList<PositionPlayer> hitters = generateHitterArray(hittersFile);
+
+    // Create center pane
+    Pane centerPane = new Pane();
+
 
     @Override
     public void start(Stage primaryStage){
+
+        // Format center pane
+        centerPane.setPrefWidth(500);
+        centerPane.setPrefHeight(500);
+        centerPane.setBorder(new Border(new BorderStroke(Color.BLACK,
+                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+        // Left pane
+        VBox vBox = new VBox();
+        Label playerSearchLabel = new Label("View Player Stats");
+        playerSearchLabel.setFont(Font.font(20));
+        Label yearLabel = new Label("Year:");
+        ComboBox<Integer> cbYear = new ComboBox<Integer>();
+        cbYear.getItems().addAll(2022);
+        Label teamLabel = new Label("Team:");
+        ComboBox<String> cbTeam = new ComboBox<String>();
+        cbTeam.getItems().addAll("Arizona Diamondbacks", "Atlanta Braves", "Baltimore Orioles", "Boston Red Sox",
+                "Chicago White Sox", "Chicago Cubs", "Cincinnati Reds", "Cleveland Guardians", "Colorado Rockies",
+                "Detroit Tigers", "Houston Astros", "Kansas City Royals", "Los Angeles Angels", "Los Angeles Dodgers",
+                "Miami Marlins", "Milwaukee Brewers", "Minnesota Twins", "New York Mets", "New York Yankees", "Oakland Athletics",
+                "Philadelphia Phillies", "Pittsburgh Pirates", "San Diego Padres", "San Francisco Giants", "Seattle Mariners",
+                "St. Louis Cardinals", "Tampa Bay Rays", "Texas Rangers", "Toronto Blue Jays", "Washington Nationals");
+
+        String [][] translateArray = generateTeamTranslateArray();
+
+        Label playerLabel = new Label("Player");
+        ComboBox<String> cbPlayer = new ComboBox<String>();
+
+        // Team combo box action
+        cbTeam.setOnAction(e -> {
+                cbPlayer.getItems().clear();
+                int index = 0;
+                for (int i = 0; i < translateArray.length; i++){
+                    if(cbTeam.getValue().equalsIgnoreCase(translateArray[i][0]))
+                        index = i;
+                }
+                for (int j = 0; j < pitchers.size(); j++) {
+                    if (translateArray[index][1].equals(pitchers.get(j).getTeam())){
+                        cbPlayer.getItems().add(pitchers.get(j).getName());
+                    }
+                }
+        });
+
+        // Player combo box action
+        cbPlayer.setOnAction(e -> {
+            displayPitcherInfo(cbPlayer.getValue(), pitchers);
+        });
+
+
+
+        vBox.setSpacing(5);
+        vBox.setAlignment(Pos.CENTER_LEFT);
+        vBox.getChildren().addAll(playerSearchLabel, yearLabel, cbYear, teamLabel, cbTeam, playerLabel, cbPlayer);
+
+        // Top pane
+        HBox hBoxTop = new HBox();
         Label title = new Label("Welcome to a Simple Stats Viewer!" );
         title.setPrefSize(500, 50);
         title.setFont(Font.font(30));
-        TextField searchField = new TextField("Search");
-        searchField.setPrefHeight(20);
+        title.setAlignment(Pos.CENTER);
+        TextField playerSearch = new TextField("Player search");
+        hBoxTop.getChildren().addAll(title);
+        hBoxTop.setAlignment(Pos.CENTER);
+
+        // Bottom pane
+        HBox hBoxBottom = new HBox();
+        Label byLine = new Label("By Nate Elison     ");
+        byLine.setFont(Font.font(10));
+        hBoxBottom.getChildren().addAll(byLine);
+        hBoxBottom.setAlignment(Pos.CENTER_RIGHT);
 
         BorderPane borderPane = new BorderPane();
-        Pane centerPane = new Pane();
-        centerPane.setPrefWidth(700);
-        centerPane.setPrefHeight(700);
+        borderPane.setRight(playerSearch);
+        borderPane.setTop(hBoxTop);
+        borderPane.setLeft(vBox);
         borderPane.setCenter(centerPane);
-        HBox hBox = new HBox();
+        borderPane.setBottom(hBoxBottom);
 
-        VBox vBox = new VBox();
-        Label playerSearch = new Label("View player stats");
-        Label year = new Label("Year");
-        ComboBox<Integer> cbYear = new ComboBox<Integer>();
-        cbYear.getItems().addAll(2022);
-
-        hBox.centerShapeProperty();
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(title, searchField);
-        borderPane.setTop(hBox);
         Scene scene = new Scene(borderPane);
-
         primaryStage.setTitle("A Simple Stats Viewer");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
+    public static String[][] generateTeamTranslateArray(){
+        String [][] translateArray =
+                {{"Arizona Diamondbacks", "ARI"},
+                        {"Atlanta Braves", "ATL"},
+                        {"Baltimore Orioles", "BAL"},
+                        {"Boston Red Sox", "BOS"},
+                        {"Chicago White Sox", "CHW"},
+                        {"Chicago Cubs", "CHC"},
+                        {"Cincinnati Reds", "CIN"},
+                        {"Cleveland Guardians", "CLE"},
+                        {"Colorado Rockies", "COL"},
+                        {"Detroit Tigers", "DET"},
+                        {"Houston Astros", "HOU"},
+                        {"Kansas City Royals", "KCR"},
+                        {"Los Angeles Angels", "LAA"},
+                        {"Los Angeles Dodgers", "LAD"},
+                        {"Miami Marlins", "MIA"},
+                        {"Milwaukee Brewers", "MIL"},
+                        {"Minnesota Twins", "MIN"},
+                        {"New York Mets", "NYM"},
+                        {"New York Yankees", "NYY"},
+                        {"Oakland Athletics", "OAK"},
+                        {"Philadelphia Phillies", "PHI"},
+                        {"Pittsburgh Pirates", "PIT"},
+                        {"San Diego Padres", "SDP"},
+                        {"San Francisco Giants", "SFG"},
+                        {"Seattle Mariners", "SEA"},
+                        {"St. Louis Cardinals", "STL"},
+                        {"Tampa Bay Rays", "TBR"},
+                        {"Texas Rangers", "TEX"},
+                        {"Toronto Blue Jays", "TOR"},
+                        {"Washington Nationals", "WSN"},
+                };
+        return translateArray;
+    }
 
+    //TODO display infos
+    public static void displayPitcherInfo(String name, ArrayList<Pitcher> pitchers){
 
+    }
 
     public static ArrayList<PositionPlayer> generateHitterArray(File file) throws FileNotFoundException {
         Scanner input = new Scanner(file);
