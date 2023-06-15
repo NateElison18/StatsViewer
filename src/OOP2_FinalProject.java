a1106106178778917f4a2726cff89e28f9374182
NateElison <122648132+NateElison18@users.noreply.github.com>
2023-06-14 19:52:49 -0600

    Update OOP2_FinalProject.java
    Stats now visible and centered.
    Started working on check boxes for different stats.

--- src/OOP2_FinalProject.java
+++ src/OOP2_FinalProject.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -131,21 +132,24 @@ public class OOP2_FinalProject extends Application {
 
 
     //TODO display infos
-    public static Pane displayPitcherInfo(Pitcher pitcher) throws FileNotFoundException {
+    public  Pane displayPitcherInfo(Pitcher pitcher) throws FileNotFoundException {
         BorderPane statsPane = new BorderPane();
 
         // Display header
         Label playerName = new Label(pitcher.getName());
         playerName.setFont(Font.font(30));
+        Label filler = new Label("");
+        filler.setFont(Font.font(30));
         String fileName = "TeamLogos/" + pitcher.getTeam() + ".png";
         FileInputStream imageInputStream = new FileInputStream(new File(fileName));
         HBox hBox = new HBox();
-//        hBox.setBorder(new Border(new BorderStroke(Color.BLACK, Color.TRANSPARENT, Color.BLACK, Color.BLACK, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID, BorderStrokeStyle.SOLID,
-//                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT, new javafx.geometry.Insets(0, 0, 0, 0))));
-        hBox.setPrefWidth(statsPane.getPrefWidth());
+
+//        hBox.setMinWidth(statsPane.getPrefWidth());
         Image teamImage = new Image(imageInputStream);
-        hBox.getChildren().addAll(new ImageView(teamImage), playerName);
+        hBox.getChildren().addAll(new ImageView(teamImage), playerName, filler);
         hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setStyle("-fx-border-color: black");
+//        hBox.setMinWidth(centerPane.getWidth());
 
         // Display stats
 //        TextArea taStats = new TextArea("Summary\tW\tL\tERA\tG\tGS\tSV\tIP\tSO\tWHIP\n" +
@@ -154,48 +158,138 @@ public class OOP2_FinalProject extends Application {
 //                + "\t" + pitcher.getInningsPitched() + "\t" + pitcher.getStrikeOuts() + "\t" + pitcher.getWhip());
 //        taStats.setEditable(false);
 
-        ObservableList<Pitcher> pitchers = FXCollections.observableArrayList(pitcher);
 
-        TableView statsTable = new TableView();
-        statsTable.setEditable(false);
-        TableColumn<Pitcher, Integer> wins = new TableColumn<>("W");
-        TableColumn<Pitcher, Integer> losses = new TableColumn<>("L");
-        TableColumn<Pitcher, Double> eRA = new TableColumn<>("ERA");
-        TableColumn<Pitcher, Integer> games = new TableColumn<>("G");
-        TableColumn<Pitcher, Integer> gamesStarted = new TableColumn<>("GS");
-        TableColumn<Pitcher, Integer> saves = new TableColumn<>("SV");
-        TableColumn<Pitcher, Double> iP = new TableColumn<>("IP");
-        TableColumn<Pitcher, Integer> sO = new TableColumn<>("Strikeouts");
-        TableColumn<Pitcher, Double> whip = new TableColumn<>("WHIP");
+//        TableView statsTable = new TableView();
+//        statsTable.setEditable(false);
+//        TableColumn<Pitcher, Integer> wins = new TableColumn<>("W");
+//        TableColumn<Pitcher, Integer> losses = new TableColumn<>("L");
+//        TableColumn<Pitcher, Double> eRA = new TableColumn<>("ERA");
+//        TableColumn<Pitcher, Integer> games = new TableColumn<>("G");
+//        TableColumn<Pitcher, Integer> gamesStarted = new TableColumn<>("GS");
+//        TableColumn<Pitcher, Integer> saves = new TableColumn<>("SV");
+//        TableColumn<Pitcher, Double> iP = new TableColumn<>("IP");
+//        TableColumn<Pitcher, Integer> sO = new TableColumn<>("Strikeouts");
+//        TableColumn<Pitcher, Double> whip = new TableColumn<>("WHIP");
+//
+////        PropertyValueFactory factory= new PropertyValueFactory<>("wins");
+//        wins.setCellValueFactory(new PropertyValueFactory<>("wins"));
+//        losses.setCellValueFactory(new PropertyValueFactory<>("losses"));
+//        eRA.setCellValueFactory(new PropertyValueFactory<>("era"));
+//        games.setCellValueFactory(new PropertyValueFactory<>("gamesPlayed"));
+//        gamesStarted.setCellValueFactory(new PropertyValueFactory<>("gamesStarted"));
+//        saves.setCellValueFactory(new PropertyValueFactory<>("saves"));
+//        iP.setCellValueFactory(new PropertyValueFactory<>("inningsPitched"));
+//        sO.setCellValueFactory(new PropertyValueFactory<>("StrikeOuts"));
+//        whip.setCellValueFactory(new PropertyValueFactory<>("whip"));
+//
+//
+//        statsTable.getColumns().addAll(wins, losses, eRA, games, gamesStarted, saves, iP, sO, whip);
+//        statsTable.getItems().add(pitcher);
 
-//        PropertyValueFactory factory= new PropertyValueFactory<>("wins");
-        wins.setCellValueFactory(new PropertyValueFactory<>("wins"));
-        losses.setCellValueFactory(new PropertyValueFactory<>("losses"));
-        eRA.setCellValueFactory(new PropertyValueFactory<>("era"));
-        games.setCellValueFactory(new PropertyValueFactory<>("gamesPlayed"));
-        gamesStarted.setCellValueFactory(new PropertyValueFactory<>("gamesStarted"));
-        saves.setCellValueFactory(new PropertyValueFactory<>("saves"));
-        iP.setCellValueFactory(new PropertyValueFactory<>("inningsPitched"));
-        sO.setCellValueFactory(new PropertyValueFactory<>("StrikeOuts"));
-        whip.setCellValueFactory(new PropertyValueFactory<>("whip"));
-
-
-        statsTable.getColumns().addAll(wins, losses, eRA, games, gamesStarted, saves, iP, sO, whip);
-        statsTable.getItems().add(pitcher);
-
+        // Build Center Pane
         GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setAlignment(Pos.TOP_CENTER);
+
+        VBox summaryBox = new VBox();
+        VBox winBox = new VBox();
+        VBox lossBox = new VBox();
+        VBox eraBox = new VBox();
+        VBox gameBox = new VBox();
+        VBox gamesStartedBox = new VBox();
+        VBox savesBox = new VBox();
+        VBox iPBox = new VBox();
+        VBox soBox = new VBox();
+        VBox whipBox = new VBox();
+
+        summaryBox.getChildren().addAll(new Label("Summary"), new Label("2022"));
+        winBox.getChildren().addAll(new Label("W"), new Label(String.valueOf(pitcher.getWins())));
+        lossBox.getChildren().addAll(new Label("L"), new Label(String.valueOf(pitcher.getLosses())));
+        eraBox.getChildren().addAll(new Label("ERA"), new Label(String.valueOf(pitcher.getEra())));
+        gameBox.getChildren().addAll(new Label("G"), new Label(String.valueOf(pitcher.getGamesPlayed())));
+        gamesStartedBox.getChildren().addAll(new Label("GS"), new Label(String.valueOf(pitcher.getGamesStarted())));
+        savesBox.getChildren().addAll(new Label("SV"), new Label(String.valueOf(pitcher.getSaves())));
+        iPBox.getChildren().addAll(new Label("IP"), new Label(String.valueOf(pitcher.getInningsPitched())));
+        soBox.getChildren().addAll(new Label("SO"), new Label(String.valueOf(pitcher.getStrikeOuts())));
+        whipBox.getChildren().addAll(new Label("WHIP"), new Label(String.valueOf(pitcher.getWhip())));
+
+        summaryBox.setAlignment(Pos.CENTER);
+        winBox.setAlignment(Pos.CENTER);
+        lossBox.setAlignment(Pos.CENTER);
+        eraBox.setAlignment(Pos.CENTER);
+        gameBox.setAlignment(Pos.CENTER);
+        gamesStartedBox.setAlignment(Pos.CENTER);
+        savesBox.setAlignment(Pos.CENTER);
+        iPBox.setAlignment(Pos.CENTER);
+        soBox.setAlignment(Pos.CENTER);
+        whipBox.setAlignment(Pos.CENTER);
+
+        gridPane.add(summaryBox, 0, 0);
+        gridPane.add(winBox, 1, 0);
+        gridPane.add(lossBox, 2, 0);
+        gridPane.add(eraBox, 3, 0);
+        gridPane.add(gameBox, 4, 0);
+        gridPane.add(gamesStartedBox, 5, 0);
+        gridPane.add(savesBox, 6, 0);
+        gridPane.add(iPBox, 7, 0);
+        gridPane.add(soBox, 8, 0);
+        gridPane.add(whipBox, 9, 0);
+
+        gridPane.setHgap(20);
+        gridPane.setTranslateY(30);
 
 
-        Pane scrollPane = new Pane(statsTable);
-        scrollPane.setMaxWidth(statsPane.getMaxWidth());
-        scrollPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-//        Pane pane = new Pane(scrollPane);
 
-//        pane.setMaxWidth(statsPane.getWidth());
 
-        statsPane.setCenter(scrollPane);
+
+//        gridPane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+
+        // Build Bottom pane
+        HBox defaultCheckBoxs = new HBox();
+        CheckBox chkWin = new CheckBox("W");
+        CheckBox chkLoss = new CheckBox("L");
+        CheckBox chkERA = new CheckBox("ERA");
+        CheckBox chkGames = new CheckBox("G");
+        CheckBox chkGamesStarted = new CheckBox("GS");
+        CheckBox chkSaves = new CheckBox("SV");
+        CheckBox chkIp = new CheckBox("IP");
+        CheckBox chkSo = new CheckBox("SO");
+        CheckBox chkWhip = new CheckBox("WHIP");
+
+        chkWin.setSelected(true);
+        chkLoss.setSelected(true);
+        chkERA.setSelected(true);
+        chkGames.setSelected(true);
+        chkGamesStarted.setSelected(true);
+        chkSaves.setSelected(true);
+        chkIp.setSelected(true);
+        chkSo.setSelected(true);
+        chkWhip.setSelected(true);
+
+        chkWin.setFont(Font.font(10));
+        chkLoss.setFont(Font.font(10));
+        chkERA.setFont(Font.font(10));
+        chkGames.setFont(Font.font(10));
+        chkGamesStarted.setFont(Font.font(10));
+        chkSaves.setFont(Font.font(10));
+        chkIp.setFont(Font.font(10));
+        chkSo.setFont(Font.font(10));
+        chkWhip.setFont(Font.font(10));
+
+        defaultCheckBoxs.getChildren().addAll(chkWin,chkLoss, chkERA, chkGames, chkGamesStarted, chkSaves, chkIp, chkSo, chkWhip);
+        defaultCheckBoxs.setSpacing(15);
+        defaultCheckBoxs.setAlignment(Pos.CENTER);
+        VBox checkBoxs = new VBox();
+        checkBoxs.getChildren().addAll(defaultCheckBoxs);
+//        checkBoxs.setStyle("-fx-border-color: black");
+        checkBoxs.setTranslateY(-5);
+
+
         statsPane.setTop(hBox);
+        statsPane.setCenter(gridPane);
+        statsPane.setBottom(checkBoxs);
+
+        statsPane.setMinHeight(centerPane.getHeight());
+        statsPane.setMinWidth(centerPane.getWidth());
 
         return statsPane;
     }
